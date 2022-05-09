2bf56addcb9e03e5d9fec52a758398b2030116f3
Andy Carrascoza <ajcarrascoza@email.neit.edu>
2022-05-09 10:39:52 -0400

    w7 assignment

---
+++ SC115/wk6/script.js
@@ -0,0 +1,23 @@
+var tray = document.querySelector(`#tray`);
+
+tray.addEventListener(`Click`, openClose)
+function openClose(e)
+{
+    var nav = document.querySelector(`nav`);
+    nav.classList.toggle(`hidden`);
+}
+
+var tabs = document.querySelectorAll(`#tabs a`);
+console.log(tabs);
+
+for(let i=0; i<tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, function(e){
+        for(let i=0; i<tabs.length; i++)
+        {
+            tabs[i].style.backgroundcolor=`orange`;
+        }
+        e.target.style.backgroundcolor=`rgb(101, 105, 97)`;
+        document.querySelectorAll(`#breadcrumbs a`)[3].innerHTML = `Tabs ${i+1}`
+    });
+}
